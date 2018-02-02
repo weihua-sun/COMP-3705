b619ed4ae09571060f90e1114d780aea5968b033
weihua-sun <sunxx645@umn.edu>
2018-02-01 17:36:58 -0700

    Completed COMP-3705 lab 09

--- server/api/users/index.js
+++ server/api/users/index.js
@@ -2,9 +2,18 @@ import express from 'express';
 import * as controller from './users.controller';
 
 let router = express.Router();
-export {router};
+
+// GET methods
 router.get('/', controller.index);
 router.get('/:id', controller.show);
+
+// POST method
 router.post('/', controller.create);
-router.put('/:id', controller.upsert);
+
+// PUT method
+router.put('/:id', controller.update);
+
+// DELETE method
 router.delete('/:id', controller.destroy);
+
+export {router};
--- server/api/users/users.model.js
+++ server/api/users/users.model.js
@@ -1,90 +1,65 @@
-import uuidv4 from 'uuid/v4';
+import mongoose from 'mongoose';
+let Schema = mongoose.Schema;
 
+/*
+  This section declares the schemas for the different documents
+  that will be used
+ */
 
-class User {
-  users = [];
+// This schema represents the address of the user
+let addressSchema = Schema({
+  // addressLine1 is a simple String type that is required
+  addressLine1: {type: String, required: true},
+  // addressLine2 is a simple String type that is NOT required
+  addressLine2: {type: String, required: false},
+  // city is a simple String type that is required
+  city: {type: String, required: true},
+  // state is a simple String type that is required
+  state: {type: String, required: true},
+  // zip is a simple Number type that is required
+  zip: {type: Number, required: true}
+});
 
-  find() {
-    // Returns a list of all users
-    return this.users;
-  }
+// This schema represents the name of the user
+let nameSchema = Schema({
+  // firstName is a simple String type that is required
+  firstName: {type: String, required: true},
+  // middleName is a simple String type that is not required
+  middleName: {type: String, required: false},
+  // lastName is a simple String type that is required
+  lastName: {type: String, required: true}
+});
 
-  findById(userId) {
-    // Find user by Id
-    // Returns user, or null if not present
-    var a = 0;
-    for(var i = 0; i < this.users.length; i++) {
-      if(this.users[i].id == userId) {
-        a += 1;
-        return this.users[i];
-      }
-    }
-    if(a == 0) {
-      return null;
-    }
-  }
+// This is the main user schema
+let userSchema = Schema({
+  // Age is a simple number type that is required
+  age: {type: Number, required: true},
+  /*
+   Address is referenced as a 'foreign key' using the objectId
+   of an address stored in a separate collection.
+   The address will be populated by Mongoose using 'Population'
+   http://mongoosejs.com/docs/populate.html
+  */
+  address: {type: Schema.Types.ObjectId, ref: 'Address'},
+  /*
+   Name is a subdocument of User, and will be stored
+   in the same document as the User itself.
+   Unlike a populated document, this doesn't require an
+   ObjectId reference and the schema for name can be
+   referenced directly
+  */
+  name: nameSchema
+});
 
-  create(user) {
-    // Create a new user
-    // Return created user
-    // Generate the id and overwrite any id that may be present in user
-    var newId = uuidv4();
-    var Newuser = ({
-      id: newId,
-      name: user.name,
-      address: user.address,
-      age: user.age,
-    });
-    this.users.push(Newuser);
-    return Newuser;
-  }
+/*
+  This section creates interactive models from the defined schemas
+  above so that you can perform Create Read Update and Delete (CRUD)
+  operations against the schemas.
+  NOTE since the nameSchema is embedded within userSchema, it does NOT have
+  to be created as a model!
+ */
+let Address = mongoose.model('Address', addressSchema);
+let User = mongoose.model('User', userSchema);
 
-  findOneAndUpdate(user) {
-    // Find user and update
-    // If user does not exist, create it using Id provided
-    // Return true if user was updated, false if user was created
-    //let existinguser = thia.findUser(user.id);
-    //
-    var updateIndex = this.users.map(function(U) {
-      return U.id;
-    }).indexOf((user.id));
-
-    if(updateIndex === -1) {
-      //Movie not found, create new
-      this.users.push({
-        id: user.id,
-        name: user.name,
-        address: user.address,
-        age: user.age
-      });
-      return false;
-    } else {
-      //Update existing
-      this.users[updateIndex] = {
-        id: user.id,
-        name: user.name,
-        address: user.address,
-        age: user.age
-      };
-      return true;
-    }
-  }
-
-  remove(user) {
-    // Remove user if exists with the Id provided
-    // Return true if removed
-    // Return false if did user not exist
-    var removeIndex = this.users.map(function(U) {
-      return U.id;
-    }).indexOf(user.id);
-
-    if(removeIndex === -1) {
-      return false;
-    } else {
-      this.users.splice(removeIndex, 1);
-      return true;
-    }
-  }
-}
-
-export default new User();
+// Export the two created models, Address and User
+export {Address, User};
