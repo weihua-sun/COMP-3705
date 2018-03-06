5a472250dd875bafccfd390298a89f957c92f84d
weihua-sun <sunxx645@umn.edu>
2018-03-05 18:10:50 -0700

    In work changes for COMP-3705 project 2

--- client/app/main/main.component.js
+++ client/app/main/main.component.js
@@ -3,15 +3,68 @@ const ngRoute = require('angular-route');
 import routing from './main.routes';
 
 export class MainController {
-
   /*@ngInject*/
-  constructor($http) {
+  constructor($http, User, Recipe) {
     this.$http = $http;
-    this.values = ['first', 'second', 'third'];
+    this.User = User;
+    this.Recipe = Recipe;
+    this.setData();
+    this.getUserData();
+    this.getRecipeData();
   }
 
-  $onInit() {
+  setData() {
+    this.values = ['first', 'second', 'third'];
+    this.valueToSquare = 4;
   }
+
+  getUserData() {
+    this.User.getAllUsers()
+      .then(response => {
+        this.users = response.data;
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }
+
+  getRecipeData() {
+    this.Recipe.getAllUsers()
+      .then(response => {
+        this.recipes = response.data;
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }
+
+}
+export function UserService($http) {
+  'ngInject';
+  var User = {
+    getAllUsers() {
+      return $http.get('/api/User/');
+    }
+  }
+  return User;
+}
+
+export function RecipeService($http) {
+  'ngInject';
+  var Recipe = {
+    getAllUsers() {
+      return $http.get('/api/Recipe/');
+    }
+  }
+  return Recipe;
+}
+
+
+export function SquareFilter() {
+  var squareFunction = function(value) {
+    return value * value;
+  }
+  return squareFunction;
 }
 
 export default angular.module('comp3705App.main', [ngRoute])
@@ -21,4 +74,8 @@ export default angular.module('comp3705App.main', [ngRoute])
     controller: MainController,
     controllerAs: 'mainController'
   })
+  .service('User', UserService)
+  .service('Recipe', RecipeService)
+  .filter('Square', SquareFilter)
   .name;
+
--- server/routes.js
+++ server/routes.js
@@ -11,7 +11,8 @@ import * as recipes from './api/recipes';
 
 export default function(app) {
   app.use('/api/recipes', recipes.router);
-  app.use('/api/User', users.router);
+  app.use('/api/users', users.router);
+
 
   // All undefined asset or api routes should return a 404
   app.route('/:url(api|auth|components|app|bower_components|assets)/*')
